af610ecd240bf0e5d99b65a44a169db41ba5c639
Batuhan TANIR <batuhan.tanir00@gmail.com>
2025-04-24 17:27:28 +0300

    part 1 complete

--- p1/installers/server.sh
+++ p1/installers/server.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y curl
 
+IP=$1
+
+RC_FILE="/home/vagrant/.bashrc"
+INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --bind-address=$IP --advertise-address=$IP --node-ip=$IP"
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC" sh -
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/token
+
+echo "alias k='kubectl'" >> "$RC_FILE"
--- p1/installers/server_worker.sh
+++ p1/installers/server_worker.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y curl
+
+MASTER_IP=$1
+NODE_IP=$2
+
+SHARED_TOKEN_URL="/vagrant/token"
+MASTER_URL="https://$MASTER_IP:6443"
+INSTALL_K3S_EXEC="--node-ip=$NODE_IP"
+
+TOKEN=$(cat "$SHARED_TOKEN_URL")
+echo "token: $TOKEN"
+echo "INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC"
+echo "MASTER_URL: $MASTER_URL"
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC" K3S_URL="$MASTER_URL" K3S_TOKEN="$TOKEN" sh -
+
+rm -f "$SHARED_TOKEN_URL"
