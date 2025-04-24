16a1f415347326342d25d75b1d6bd4db8b78c529
emiryasa <emirys774@gmail.com>
2025-04-24 21:07:57 +0300

    p3

---
+++ p3/install.sh
@@ -0,0 +1,29 @@
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
+
+#sudo apt-get install ca-certificates curl
+#sudo install -m 0755 -d /etc/apt/keyrings
+#sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+#sudo chmod a+r /etc/apt/keyrings/docker.asc
+#echo \
+  #"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  #$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  #sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+k3d cluster create --config ./configs/cluster.yaml
+kubectl cluster-info
+
+kubectl create namespace argocd
+kubectl create namespace dev
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl wait --for=condition=ready pod --all -n argocd --timeout=300s
+
+kubectl apply -n argocd -f ./configs/application.yaml
+kubectl apply -n dev -f./configs/deployment.yaml
+
+echo "Password: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)"
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
