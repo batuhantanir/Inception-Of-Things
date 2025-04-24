6b556cde60d830a6eeaf61907f46ea7dc36b4539
Batuhan TANIR <batuhan.tanir00@gmail.com>
2025-04-24 19:13:30 +0300

    complete part2

---
+++ p2/installers/server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y curl
+
+IP=$1
+
+RC_FILE="/home/vagrant/.bashrc"
+INSTALL_K3S_EXEC="--write-kubeconfig-mode=644"
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC" sh -
+
+echo "alias k='kubectl'" >> "$RC_FILE"
+
+sleep 20
+
+kubectl apply -n kube-system -f /vagrant/app-one/deployment.yaml
+kubectl apply -n kube-system -f /vagrant/app-two/deployment.yaml
+kubectl apply -n kube-system -f /vagrant/app-three/deployment.yaml
+kubectl apply -n kube-system -f /vagrant/network/ingress.yaml
